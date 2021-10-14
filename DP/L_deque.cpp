d5fec40a3df9506cc3c8fb5cca56bc89d2e7f4da
i-anandsingh <singh.anand974@gmail.com>
2021-10-14 21:45:25 +0530

    maximizing pattern

--- DP/L_deque.cpp
+++ DP/L_deque.cpp
@@ -1,4 +1,4 @@
-// Problem Link :- https://www.codechef.com/problems/BPHC03
+// Problem Link :- https://atcoder.jp/contests/dp/tasks/dp_l
 /* By- Anand Singh */    
 #include<bits/stdc++.h>
 //#include <ext/pb_ds/assoc_container.hpp>
@@ -35,59 +35,27 @@ typedef vector<pii>    vp;
 //typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
 //typedef trie<string,null_type,trie_string_access_traits<>,pat_trie_tag,trie_prefix_search_node_update> pbtrie;
 
-void file_i_o(){
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    #ifndef ONLINE_JUDGE
-        freopen("input.txt","r",stdin);
-        freopen("output.txt","w",stdout);
-    #endif
-}
-
-int cnt = 0;
-
-bool isSafe(int i, int j, vector<vector<int>> &board, vector<vector<bool>> &vis){
-	int n = board.size();
-	return i >= 0 and i < n and j >= 0 and j < n and (board[i][j] != 1) and (vis[i][j] == false);
-}
-
-void unique_path(int cr, int cc, vector<vector<int>> &board, vector<vector<bool>> &vis, int n){
-	if( cr == n-1 and cc == n-1){
-		cnt++;
-		return;
-	}
-
-	vis[cr][cc] = true;
-	if(isSafe(cr+1, cc, board, vis))
-		unique_path(cr+1, cc, board, vis, n);
-
-	if(isSafe(cr-1, cc, board, vis))
-		unique_path(cr-1, cc, board, vis, n);
-
-	if(isSafe(cr, cc+1, board, vis))
-		unique_path(cr, cc+1, board, vis, n);
-
-	if(isSafe(cr, cc-1, board, vis))
-		unique_path(cr, cc-1, board, vis, n);
-
-	vis[cr][cc] = false;
+void err(std::istream_iterator<std::string> it) {}
+template<typename T, typename... Args>
+void err(std::istream_iterator<std::string> it, T a, Args... args) {
+    std::cout << *it << " = " << a << std::endl;
+    err(++it, args...);
 }
 
 signed main(int32_t argc, char const *argv[]){
-	// getFast;
-	file_i_o();
+	getFast;
 	//Write your code here...
 	int n;
 	cin>>n;
-	vector<vector<int>> board(n, vector<int> (n));
-	vector<vector<bool>> vis(n, vector<bool> (n, false));
-	for(int i = 0; i < n; i++){
-		for(int j = 0; j < n; j++){
-			cin>>board[i][j];
+	vector<int> a(n+1);
+	loop(i, 1, n)	cin>>a[i];
+	vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
+	for(int i = n; i >= 1; i--){
+		for(int j = i; j <= n; j++){
+			if(i == j)	dp[i][j] = a[i];
+			else	dp[i][j] = max(a[i]-dp[i+1][j], a[j]-dp[i][j-1]);
 		}
 	}
-	unique_path(0, 0, board, vis, n);
-	cout<<cnt<<br;
+	cout<<dp[1][n]<<br;
 	return 0;
 }
